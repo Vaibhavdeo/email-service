608f1e5dd2578ab961eefedd4eec7f53fd0542a7
Vaibhavdeo <vaibhavdeore5@gmail.com>
2019-03-14 12:48:45 +0530

    Update EmailControllerLogic.java

--- src/main/java/com/email/greeting/app/service/EmailControllerLogic.java
+++ src/main/java/com/email/greeting/app/service/EmailControllerLogic.java
@@ -28,7 +28,7 @@ public class EmailControllerLogic {
 	 * 
 	 * @throws MessagingException
 	 */
-	@Scheduled(cron = "0 0 10 * * ?")
+	@Scheduled(cron = "* */1 * * * *")
 	public void getAllDetails() throws MessagingException {
 		System.out.println("Cron method executed");
 		restTemplate = new RestTemplate();
